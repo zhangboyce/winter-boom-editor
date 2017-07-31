22f3cc44409f6e6338403166791d9312f4c25c36
jame <Jame.wu@ccegroup.cn>
2017-07-31 14:43:24 +0800

    上传图片限制在2m

--- app/utils/upload.js
+++ app/utils/upload.js
@@ -4,44 +4,54 @@ let body = $('body');
 
 export default function ({name, url, change, success, error, timeout}) {
 
-    let input = $(`<input type="file" accept="image/jpg,image/jpeg,image/png,image/gif" name="${name || 'file'}"/>`).hide().insertAfter(body);
+    let input = $(`<input type="file" id="upFile" accept="image/jpg,image/jpeg,image/png,image/gif" name="${name || 'file'}"/>`).hide().insertAfter(body);
     let iframe = $('<iframe src="javascript:void(0);"></iframe>').hide().appendTo(body)[0];
     let getBody = () => $((iframe.contentDocument || iframe.contentWindow.document).body);
 
-    let extra = { };
+    let extra = {};
     input.change(e => {
-        let form = $('<form method="post" enctype="multipart/form-data"></form>');
-        let iframeBody = getBody();
-        iframeBody.empty();
-        form.appendTo(iframeBody);
-        form.attr('action', typeof(url) === 'string'? url: url());
-        form.append(input.clone());
+        var inputUpFile = document.getElementById("upFile");
+        if (inputUpFile.files) {
+            let f = inputUpFile.files[0].size / 1024;
+            if (f < 1024 * 2) {
+                let form = $('<form method="post" enctype="multipart/form-data"></form>');
+                let iframeBody = getBody();
+                iframeBody.empty();
+                form.appendTo(iframeBody);
+                form.attr('action', typeof(url) === 'string' ? url : url());
+                form.append(input.clone());
 
-        for (let k in extra) {
-            if (extra.hasOwnProperty(k)) {
-                form.append($(`<input type='text' name='${ k }' value='${ extra[k] }'>`));
+                for (let k in extra) {
+                    if (extra.hasOwnProperty(k)) {
+                        form.append($(`<input type='text' name='${ k }' value='${ extra[k] }'>`));
+                    }
+                }
+
+                if (change ? change() : true) {
+                    form.submit();
+                    let checkCount = 0;
+                    let checkResult = () => {
+                        let result = getBody().text();
+                        checkCount++;
+                        if (result.startsWith('success:')) {
+                            success && success(result.substring(8));
+                        } else if (result.startsWith('error:')) {
+                            error && error(result.substring(6));
+                        } else if (checkCount < 50) {
+                            setTimeout(checkResult, 100)
+                        } else {
+                            timeout && timeout();
+                        }
+                    };
+
+                    checkResult();
+                }
+
+            } else {
+                alert("图片不能大于2M");
             }
         }
 
-        if(change? change() : true) {
-            form.submit();
-            let checkCount = 0;
-            let checkResult = () => {
-                let result = getBody().text();
-                checkCount ++;
-                if(result.startsWith('success:')) {
-                    success && success(result.substring(8));
-                }else if(result.startsWith('error:')){
-                    error && error(result.substring(6));
-                }else if(checkCount < 50){
-                    setTimeout(checkResult, 100)
-                }else {
-                    timeout && timeout();
-                }
-            };
-
-            checkResult();
-        }
     });
 
     return {
